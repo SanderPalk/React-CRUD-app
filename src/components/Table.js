42e5c051e27405550fd6c9b9703e2ff4f00299d0
SPalk <95194970+SanderPalk@users.noreply.github.com>
2022-09-18 17:39:39 +0300

    The table contains list of cities

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,43 @@
 import "../styles/App.css"
+import React, { useState, useEffect } from "react";
 
 function Table() {
+    const [data, getData] = useState([])
+    const url = 'http://localhost:5000/cities';
+
+    useEffect( () => {
+        fetchData()
+    }, [])
+
+    const fetchData = () => {
+        fetch(url)
+            .then((res) =>
+                res.json())
+
+            .then((response) => {
+                console.log(response);
+                getData(response);
+            })
+    }
+
     return (
         <div className="Table">
+            <h1>Visited city list</h1>
             <table>
                 <tr>
+                    <th>Id</th>
                     <th>City name</th>
                     <th>Country</th>
                     <th>Date visited</th>
                 </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                </tr>
+                {data.map((item, i) => (
+                    <tr key={i}>
+                        <td>{item.id}</td>
+                        <td>{item.name}</td>
+                        <td>{item.country}</td>
+                        <td>{item.date}</td>
+                    </tr>
+                ))}
             </table>
         </div>
     )
