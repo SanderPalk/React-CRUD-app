75041463b105e9ba3863acfdc73d0410f403e585
Sander <sanderpalk0@gmail.com>
2022-11-15 11:18:57 +0200

    Given I load the main page I see a table with the following fields id, title, url ( as picture)

--- src/App.js
+++ src/App.js
@@ -1,101 +1,26 @@
 import React, { useEffect, useState } from "react";
-import {AddPicture} from "./components/addPicture";
 import {Picture} from "./components/Home";
 
 export default function App() {
     const [photos, setPhotos] = useState([]);
+
+    const api = "https://jsonplaceholder.typicode.com/photos"
 
     useEffect(() => {
         fetchData();
     }, []);
 
     const fetchData = async () => {
-        await fetch("https://jsonplaceholder.typicode.com/photos?_limit=10")
+        await fetch(api + "?_limit=10")
             .then((response) => response.json())
             .then((data) => setPhotos(data))
             .catch((error) => console.log(error));
     };
 
-    const onAdd = async (title, url) => {
-        await fetch("https://jsonplaceholder.typicode.com/photos", {
-            method: "POST",
-            body: JSON.stringify({
-                title: title,
-                url: url,
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
-            .then((response) => {
-                if (response.status !== 201) {
-                    return;
-                } else {
-                    return response.json();
-                }
-            })
-            .then((data) => {
-                setPhotos((comments) => [...comments, data]);
-            })
-            .catch((error) => console.log(error));
-    };
-
-    const onEdit = async (id, title, url) => {
-        await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`, {
-            method: "PUT",
-            body: JSON.stringify({
-                title: title,
-                url: url,
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
-            .then((response) => {
-                if (response.status !== 200) {
-                    return;
-                } else {
-                    return response.json();
-                }
-            })
-            .then((data) => {
-                // setUsers((users) => [...users, data]);
-                const updatedPhotos = photos.map((comment) => {
-                    if (comment.id === id) {
-                        comment.name = title;
-                        comment.email = url;
-                    }
-
-                    return comment;
-                });
-
-                setPhotos((photos) => updatedPhotos);
-            })
-            .catch((error) => console.log(error));
-    };
-
-    const onDelete = async (id) => {
-        await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`, {
-            method: "DELETE"
-        })
-            .then((response) => {
-                if (response.status !== 200) {
-                    return;
-                } else {
-                    setPhotos(
-                        photos.filter((comment) => {
-                            return comment.id !== id;
-                        })
-                    );
-                }
-            })
-            .catch((error) => console.log(error));
-    };
 
     return (
         <div className="App">
             <h1>Photos</h1>
-            <AddPicture onAdd={onAdd}/>
             <div className="form_body">
                 {photos.map((photo) => (
                     <Picture
@@ -103,8 +28,8 @@ export default function App() {
                         key={photo.id}
                         title={photo.title}
                         url={photo.url}
-                        onEdit={onEdit}
-                        onDelete={onDelete}
+                        onEdit={"onEdit"}
+                        onDelete={"onDelete"}
                     />
                 ))}
             </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,40 +1,13 @@
 import React, { useState } from "react";
 
-export const Picture = ({ title, url, id, onEdit, onDelete }) => {
-    const [isEdit, setIsEdit] = useState(false);
-
-    const handleEdit = () => {
-        setIsEdit(!isEdit);
-    };
-
-    const handleDelete = () => {
-        onDelete(id);
-    };
-
-    const handleOnEditSubmit = (evt) => {
-        evt.preventDefault();
-        onEdit(id, evt.target.title.value, evt.target.url.value);
-        setIsEdit(!isEdit);
-    };
+export const Picture = ({ title, url}) => {
 
     return (
         <div>
-            {isEdit ? (
-                <form onSubmit={handleOnEditSubmit}>
-                    <input placeholder="Title" name="title" defaultValue={title} />
-                    <input placeholder="Url" name="url" defaultValue={url} />
-                    <button onSubmit={handleOnEditSubmit}>Save</button>
-                </form>
-            ) : (
-                <div className="photo">
-                    <span className="title">{title}</span>
-                    <img src={url} width={"10%"} height={"10%"}/>
-                    <div>
-                        <button onClick={handleEdit}>Edit</button>
-                        <button onClick={handleDelete}>Delete</button>
-                    </div>
-                </div>
-            )}
+            <div className="photo">
+                <span className="title">{title}</span>
+                <img src={url} width={"10%"} height={"10%"}/>
+            </div>
         </div>
     );
 };
